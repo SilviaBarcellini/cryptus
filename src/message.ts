9352a3f2ff8e799a66dc0b9f5963bd7600b9cd4f
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-02 13:40:01 +0100

    ✨ add message

---
+++ src/message.ts
@@ -0,0 +1,19 @@
+import chalk from "chalk";
+
+import { askForPasswordValue } from "./questions";
+
+export const printWelcomeMessage = () => {
+  console.log(`Welcome to ${chalk.underline.bgMagenta("Cryptus")} 🔐`);
+};
+
+export const printNoAccess = () => {
+  console.warn(chalk.bgRed("Wrong master password! Please try again"));
+};
+
+export const printPasswordSet = (passwordName: string) => {
+  console.log(`You set a new ${passwordName} password.`);
+};
+
+export const printPassword = (passwordName: string, PasswordValue: string) => {
+  console.log(`Your ${passwordName} password is ${PasswordValue}!`);
+};
