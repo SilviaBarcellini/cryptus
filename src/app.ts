b7260fadd9f8e2527805826c9fa8d9142da446ba
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-04 01:03:58 +0100

    ✨ implement CRUD

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,43 @@
-import prompts from "prompts";
 import chalk from "chalk";
 import { printWelcomeMessage, printCannotEnter } from "./message";
-import { askForAction, askForCredentials } from "./questions";
+import {
+  askForAction,
+  askForCredentials,
+  askForPasswordValue,
+} from "./questions";
 import { handleGetPassword, handleSetPassword, canEnter } from "./command";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import {
+  closeDB,
+  connectDB,
+  createPasswordDoc,
+  readPasswordDoc,
+  updatePasswordDoc,
+} from "./db";
+import { listenerCount } from "events";
+
+dotenv.config();
 
 const run = async () => {
-  printWelcomeMessage();
+  const url = process.env.MONGODB_URL;
+
+  try {
+    await connectDB(url, "cryptus-silvia");
+    await createPasswordDoc({ name: "Silvia", value: "1234" });
+    console.log(await readPasswordDoc("Silvia"));
+    console.log(await updatePasswordDoc);
+    await closeDB();
+  } catch (error) {
+    console.error(error);
+  }
+  /*  printWelcomeMessage();
   const credentials = await askForCredentials();
-  if (!canEnter(credentials.masterPassword)) {
-    printCannotEnter();
+  if (!hasAccess(credentials.masterPassword)) {
+    printNoAccess();
     run();
     return;
   }
-
   const action = await askForAction();
   switch (action.command) {
     case "set":
@@ -21,7 +46,6 @@ const run = async () => {
     case "get":
       handleGetPassword(action.passwordName);
       break;
-  }
+  } */
 };
-
 run();
